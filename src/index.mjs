be0d7d72865ee291b7878da7e414f7b4cb38cde1
EscapedGibbon <maximgolubev2@gmail.com>
2022-07-13 22:56:25 +0200

    feat:testing version of storing data inside database

--- src/index.mjs
+++ src/index.mjs
@@ -4,20 +4,24 @@
  */import fetch from "node-fetch";
 import delay from 'delay';
 import config from "./config.json" assert {type: 'json'};
+import { insertData } from "./sample.mjs"
 
 const apiKey = config.apiKey;
 
-export async function doAll() {
-
-
+async function doAll() {
 	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=46.52&lon=6.632&appid=${apiKey}`)
 		.then(async response => await response.json())
 		.then(async data => {
-			await console.log(data.main)
+			let temp = await data.main.temp;
+			await console.log(temp);
+			await insertData(temp);
+
 		});
+
 	await delay(300 * 1000);
 
 }
+
 while (true) {
 	await doAll();
 }
--- src/sample.mjs
+++ src/sample.mjs
@@ -1,9 +1,19 @@
-import sqlite3 from 'better-sqlite3'
-const db = new sqlite3('./database.db');
+import sqlite3 from 'sqlite3'
 
-db.exec("CREATE TABLE IF NOT EXISTS logs (date DATE, temperature INT)");
-db.exec("CREATE INDEX IF NOT EXISTS dateIndex ON logs (date DESC)");
-console.log(db);
-const insert = db.prepare("INSERT INTO logs VALUES (@date,@temperature");
-let temp = 33;
-insert.run({ date: Date.now(), temperature: temp });
+const db = new sqlite3.Database("./tempData.sql");
+
+// db.exec(`CREATE TABLE logs (date, temperature)`);
+export function insertData(data) {
+	const sql = `INSERT INTO logs(date, temperature)
+	VALUES(?,?)`;
+	db.run(sql, [Date.now(), 22], (err) => {
+		if (err) return console.error(err.message);
+
+		console.log("Success");
+	});
+}
+
+
+
+
+
