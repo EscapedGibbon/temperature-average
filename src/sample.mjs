d7a8a779e3d6067e63342584ace9f5b4bc047b83
EscapedGibbon <maximgolubev2@gmail.com>
2022-07-13 23:26:06 +0200

    fix: fixing display of date and temperature on a database

--- src/sample.mjs
+++ src/sample.mjs
@@ -2,15 +2,18 @@ import sqlite3 from 'sqlite3'
 
 const db = new sqlite3.Database("./tempData.sql");
 
-// db.exec(`CREATE TABLE logs (date, temperature)`);
+
 export function insertData(data) {
 	const sql = `INSERT INTO logs(date, temperature)
 	VALUES(?,?)`;
-	db.run(sql, [Date.now(), 22], (err) => {
+	db.run(sql, [new Date().toLocaleString('fr-CH'), data], (err) => {
 		if (err) return console.error(err.message);
 
 		console.log("Success");
+
 	});
+
+
 }
 
 
