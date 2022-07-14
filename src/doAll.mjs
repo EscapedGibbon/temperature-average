b9340c7842fbec0e157d06def514c4c1186fae01
EscapedGibbon <maximgolubev2@gmail.com>
2022-07-14 15:58:40 +0200

    doc:adding some comments

--- src/doAll.mjs
+++ src/doAll.mjs
@@ -6,19 +6,18 @@ import { insertData } from './insertData.mjs';
 const apiKey = config.apiKey;
 
 async function doAll() {
-	fetch(
-		`https://api.openweathermap.org/data/2.5/weather?lat=46.52&lon=6.632&appid=${apiKey}`,
-	)
-		.then(async (response) => await response.json())
-		.then(async (data) => {
-			while (true) {
-				let temp = data.main.temp;
-				console.log(temp);
-				await insertData(temp);
-				await delay(300 * 1000);
-			}
-		});
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=46.52&lon=6.632&appid=${apiKey}`,
+  )
+    .then(async (response) => await response.json())
+    .then(async (data) => {
+      while (true) {
+        let temp = data.main.temp;
+        console.log(temp);
+        await insertData(temp);
+        await delay(300 * 1000);
+      }
+    });
 }
 
 await doAll();
-
--- src/insertData.mjs
+++ src/insertData.mjs
@@ -1,9 +1,13 @@
 import Database from 'better-sqlite3';
+/**
+ * Inserts data inside the table
+ * @param {number} data - data inserted
+ */
 
 const db = new Database('./tempData.sql');
 
 export function insertData(data) {
-	const sql = db.prepare(`INSERT INTO logs(date, temperature)
+  const sql = db.prepare(`INSERT INTO logs(date, temperature)
 	VALUES(@date,@temperature)`);
-	sql.run([new Date().toLocaleString('fr-CH'), data],);
+  sql.run([new Date().toLocaleString('fr-CH'), data]);
 }
