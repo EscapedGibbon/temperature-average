b3d8eeb253ca7cf3b4223e84248ed8545be1f6e3
EscapedGibbon <maximgolubev2@gmail.com>
2022-07-16 17:00:21 +0200

    refactor: run through prettier

--- src/avgTemp.mjs
+++ src/avgTemp.mjs
@@ -1,5 +1,5 @@
 export function avgTemp(totalTemp, totalIndx) {
-	let avgTemp = totalTemp / totalIndx;
-	console.log(avgTemp);
-	return avgTemp;
+  let avgTemp = totalTemp / totalIndx;
+  console.log(avgTemp);
+  return avgTemp;
 }
--- src/insertData.mjs
+++ src/insertData.mjs
@@ -1,20 +1,20 @@
 import Database from 'better-sqlite3';
-import { avgTemp } from './avgTemp.mjs'
+import { avgTemp } from './avgTemp.mjs';
 /**
  * Inserts data inside the table
-*/
+ */
 
 const db = new Database('./tempData.sql');
 
 export function insertData(data) {
-	const insert = db.prepare(`INSERT INTO logs(date, temperature)
+  const insert = db.prepare(`INSERT INTO logs(date, temperature)
 	VALUES(?,?)`);
-	let select = db.prepare(`SELECT SUM(temperature) as sum FROM logs`);
-	let count = db.prepare(`SELECT COUNT(*) as count FROM logs`);
+  let select = db.prepare(`SELECT SUM(temperature) as sum FROM logs`);
+  let count = db.prepare(`SELECT COUNT(*) as count FROM logs`);
 
-	insert.run([new Date().toLocaleString('fr-CH'), data]);
-	let sum = select.get().sum;
-	let totalCount = count.get().count;
-	console.log(totalCount);
-	avgTemp(sum, totalCount)
+  insert.run([new Date().toLocaleString('fr-CH'), data]);
+  let sum = select.get().sum;
+  let totalCount = count.get().count;
+  console.log(totalCount);
+  avgTemp(sum, totalCount);
 }
