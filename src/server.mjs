0e77caa358a2623e4c2508a15e5d6d32cd0a5024
EscapedGibbon <maximgolubev2@gmail.com>
2022-09-01 14:36:22 +0200

    fix:changed the ip adress to listen to

--- src/server.mjs
+++ src/server.mjs
@@ -28,6 +28,6 @@ fastify.get('/avgPerHour', (req, res) => {
 
 // Run the server!
 const start = async () => {
-  await fastify.listen({ port: 3000, host: '0.0.0.0' });
+  await fastify.listen({ port: 8000, host: '0.0.0.0' });
 };
 start();
